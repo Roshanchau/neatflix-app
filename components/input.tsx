a2571094704a3d085a45b5d380f4dac773b867a1
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2023-06-06 01:35:38 +0545

    added background image , logo /added comp for auth

---
+++ components/input.tsx
@@ -0,0 +1,11 @@
+const Input = () => {
+    return (  
+        <input 
+            className="
+            
+            "
+        />
+    )
+}
+ 
+export default Input;
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,23 @@
+import Input from "@/components/input";
+
 const Auth = () => {
     return ( 
-        <div >
-            
+        <div className="relative h-screen w-screen bg-[url('../public/images/hero.jpg')] bg-no-reapeat bg-center bg-fixed bg-cover">
+            <div className="bg-black w-full h-full lg:bg-opacity-50 ">
+                <nav className="px-12 py-5">
+                    <img src="/images/logo.png" alt="logo" className="h-12"/>
+                </nav>
+                <div className="flex justify-center">
+                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                        <h2 className="text-white text-4xl mb-8 font-semibold">
+                            Sign in
+                        </h2>
+                        <div className="flex flex-col gap-4">
+                            <Input/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
      );
 }
