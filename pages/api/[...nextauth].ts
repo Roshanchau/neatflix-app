bfe0700300b2279e23f7e538bc6b1ea29a9b8ed5
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2023-06-12 03:23:47 +0545

    nextAuth done

--- pages/api/[...nextauth].ts
+++ pages/api/[...nextauth].ts
@@ -47,4 +47,15 @@ export default NextAuth({
       },
     }),
   ],
+  pages:{
+    signIn: "/auth",
+  },
+  debug: process.env.NODE_ENV==="development",
+  session:{
+    strategy: "jwt",
+  },
+  jwt:{
+    secret:process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 });
